39bb6eb84f8a85a57b82adf8b8eea44e3e79e963
Andrei137 <andrei.fabian188@gmail.com>
2025-11-09 11:02:53 +0200

    [FEAT] Add info endpoint

--- src/services/pcgw.ts
+++ src/services/pcgw.ts
@@ -1,15 +1,22 @@
 import fetch from 'node-fetch';
+import { JSDOM } from 'jsdom';
 import errors from '@media-master/http-errors';
 import {
-    MediaOption,
     Query,
+    GameInfo,
+    Platform,
+    MediaOption,
     GamesResponse,
     GameResponse,
+    Requirements,
+    PcgwParseResponse
 } from '@types';
 
 export default class PcgwService {
+    private readonly queries: string[] = ['windows', 'os_x', 'linux'];;
+
     private getOptions = async (name: string): Promise<MediaOption[]> => {
-        // some games can't be found when the name contains -
+        // Some games can't be found when the name contains -
         const gameName = name.replaceAll('-', ' ');
 
         const response = await fetch(new URL(`https://www.pcgamingwiki.com/w/api.php?action=query&format=json&list=search&srsearch=${gameName}`));
@@ -45,8 +52,63 @@ export default class PcgwService {
             }, []);
     };
 
-    private getInfo = async (id: string): Promise<object> => {
-        return {};
+    private getInfo = async (gameName: string): Promise<GameInfo> => {
+        if (!gameName) throw errors.notFound('Game not found');
+
+        const apiUrl = 'https://www.pcgamingwiki.com/w/api.php' +
+            `?action=parse&format=json&prop=text&page=${encodeURIComponent(gameName.replaceAll(' ', '_'))}`;
+
+        const response = await fetch(apiUrl);
+        if (!response.ok) throw errors.notFound('Game not found');
+
+        const json = (await response.json()) as PcgwParseResponse;
+        if (!json.parse?.text?.['*']) throw errors.notFound('Game not found');
+
+        const html = json.parse.text['*'];
+        const document = new JSDOM(html).window.document;
+        const link = `https://www.pcgamingwiki.com/wiki/${encodeURIComponent(gameName.replaceAll(' ', '_'))}`;
+
+        const gameInfo = {
+            link,
+            ...Object.fromEntries(this.queries.map(q => [q, {}])),
+        } as GameInfo;
+
+        for (const query of this.queries) {
+            const sysReqsTable = document.querySelector(`#table-sysreqs-${query}`);
+            if (!sysReqsTable) continue;
+
+            const sysReqs: Requirements = {};
+            const remapCategory = (category: string): string =>
+                category === 'HDD/SSD' ? 'space' : category.toLowerCase();
+
+            sysReqsTable
+                .querySelectorAll('.template-infotable-body, .table-sysreqs-body-row')
+                .forEach(row => {
+                    const fullCategory =
+                        row
+                            .querySelector('.table-sysreqs-body-parameter')
+                            ?.textContent?.trim() ?? '';
+
+                    let category = fullCategory;
+                    if (fullCategory.includes('(')) {
+                        category = fullCategory.split('(')[1].split(')')[0];
+                    }
+                    category = remapCategory(category);
+
+                    sysReqs[category as keyof Requirements] = {
+                        minimum: row
+                            .querySelector('.table-sysreqs-body-minimum')
+                            ?.textContent?.trim() ?? '',
+                        recommended: row
+                            .querySelector('.table-sysreqs-body-recommended')
+                            ?.textContent?.trim() ?? '',
+                    };
+                });
+
+            gameInfo[query as Platform] = sysReqs;
+        }
+
+        return gameInfo;
     };
 
     public handle = async (method: string, query: Query): Promise<unknown> => {
@@ -57,7 +119,7 @@ export default class PcgwService {
 
         if (!(method in methodMap)) {
             throw errors.notFound(
-                'Invalid endpoint! Use /api/[options|info]'
+                'Invalid endpoint! Use /[options|info]'
             );
         }
 
--- src/types/pcgw.types.ts
+++ src/types/pcgw.types.ts
@@ -19,3 +19,34 @@ interface PcgwQuery {
 export interface GamesResponse {
     query?: PcgwQuery
 };
+
+interface Spec {
+    minimum: string,
+    recommended: string,
+};
+
+export interface Requirements {
+    target?: Spec,
+    os?: Spec,
+    cpu?: Spec,
+    ram?: Spec,
+    space?: Spec,
+    gpu?: Spec,
+};
+
+export interface PcgwParseResponse {
+    parse?: {
+        text?: {
+            ['*']?: string;
+        };
+    };
+}
+
+export type Platform = 'windows' | 'os_x' | 'linux';
+
+export type GameInfo = {
+    link: string;
+} & {
+    [key in Platform]: Requirements;
+};
+
