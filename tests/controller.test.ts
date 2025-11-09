43f56c198fa7054627d2b670a90ac23644892346
Andrei137 <andrei.fabian188@gmail.com>
2025-11-09 09:40:34 +0200

    [TEST] Add tests for the options endpoint

---
+++ tests/controller.test.ts
@@ -0,0 +1,32 @@
+import runMetadataTests, { Fields } from '@media-master/metadata-service-tests';
+import { Express } from 'express';
+import { describe } from 'vitest';
+import app from '../src/app';
+
+const server = app as Express;
+
+describe('Controller', () => {
+    const endpoint: string = '/';
+    const validMap: object = {
+        'Hollow Knight': 'Hollow Knight',
+        'God Of War': 'God of War',
+        'League Of Legends': 'League of Legends',
+    };
+    const invalidMap: object = {
+        'adasdasa': '-1',
+        '' : 'Not a game',
+        'nonExistentMovie': 'nonExistentId',
+    };
+    const fieldsMap: Record<string, Fields> = {
+        options: {
+            name: { type: 'string' },
+        },
+    };
+
+    runMetadataTests(
+        server,
+        endpoint,
+        { validMap, invalidMap, fieldsMap, type: 'game' }
+    );
+});
+
